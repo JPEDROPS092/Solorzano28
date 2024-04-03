a5de0e6b986c8f5049b352b54ef8c9649e93e5cf
João Pedro <jpedropsss@gmail.com>
2024-04-03 01:27:12 -0400

    te amo mais

--- effect.js
+++ effect.js
@@ -29,22 +29,27 @@ $('document').ready(function(){
 		});
 	});
 	$('#play').click(function(){
-        var audio = $('.song')[0];
+		var audio = $('.song')[0];
         audio.play();
         $('#bulb_yellow').addClass('bulb-glow-yellow-after');
-        $('#bulb_red').addClass('bulb-glow-red-after');
-        $('#bulb_blue').addClass('bulb-glow-blue-after');
-        $('#bulb_green').addClass('bulb-glow-green-after');
-        $('#bulb_pink').addClass('bulb-glow-pink-after');
-        $('#bulb_orange').addClass('bulb-glow-orange-after');
-        $('body').css('background-color','#FFF'); // Corrigido de 'backgroud-color' para 'background-color'
-        $('body').addClass('peach-after');
-        $(this).fadeOut('slow').delay(6000).promise().done(function(){
-            $('#bannar_coming').fadeIn('slow');
-        });
-    })
-	
+		$('#bulb_red').addClass('bulb-glow-red-after');
+		$('#bulb_blue').addClass('bulb-glow-blue-after');
+		$('#bulb_green').addClass('bulb-glow-green-after');
+		$('#bulb_pink').addClass('bulb-glow-pink-after');
+		$('#bulb_orange').addClass('bulb-glow-orange-after');
+		$('body').css('backgroud-color','#FFF');
+		$('body').addClass('peach-after');
+		$(this).fadeOut('slow').delay(6000).promise().done(function(){
+			$('#bannar_coming').fadeIn('slow');
+		});
+	});
 
+	$('#bannar_coming').click(function(){
+		$('.bannar').addClass('bannar-come');
+		$(this).fadeOut('slow').delay(6000).promise().done(function(){
+			$('#balloons_flying').fadeIn('slow');
+		});
+	});
 
 	function loopOne() {
 		var randleft = 1000*Math.random();
@@ -133,31 +138,26 @@ $('document').ready(function(){
 		});
 	});
 
-		
 	$('#wish_message').click(function(){
-		 vw = $(window).width()/2;
-
-		$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
-		$('#b1').attr('id','b11');
-		$('#b2').attr('id','b22')
-		$('#b3').attr('id','b33')
-		$('#b4').attr('id','b44')
-		$('#b5').attr('id','b55')
-		$('#b6').attr('id','b66')
-		$('#b7').attr('id','b77')
-		$('#b11').animate({top:240, left: vw-350},500);
-		$('#b22').animate({top:240, left: vw-250},500);
-		$('#b33').animate({top:240, left: vw-150},500);
-		$('#b44').animate({top:240, left: vw-50},500);
-		$('#b55').animate({top:240, left: vw+50},500);
-		$('#b66').animate({top:240, left: vw+150},500);
-		$('#b77').animate({top:240, left: vw+250},500);
+		vw = $(window).width()/2;
+	
+		$('#b1,#b2,#b3,#b4,#b5').stop();
+		$('#b1').animate({top:180, left: vw-180},500); // Movendo para cima
+		$('#b2').animate({top:180, left: vw-100},500); // Movendo para cima
+		$('#b3').animate({top:180, left: vw-20},500); // Movendo para cima
+		$('#b4').animate({top:180, left: vw+60},500); // Movendo para cima
+		$('#b5').animate({top:180, left: vw+140},500); // Movendo para cima
+	
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(3000);
 		$(this).fadeOut('slow').delay(3000).promise().done(function(){
 			$('#story').fadeIn('slow');
 		});
 	});
+	
+	
+	
+	
 	
 	$('#story').click(function(){
 		$(this).fadeOut('slow');
@@ -166,45 +166,32 @@ $('document').ready(function(){
 		});
 		
 		var i;
-
+	
 		function msgLoop (i) {
-			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
-			i=i+1;
-			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
-			if(i==50){
-				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
-					$('.cake').fadeIn('fast');
-				});
-				
-			}
-			else{
-				msgLoop(i);
-			}			
-
-		});
-			// body...
+			$("p:nth-child("+i+")").fadeOut('slow').delay(1500).promise().done(function(){ // Aumentei o tempo de espera para 1500ms
+				i=i+1;
+				$("p:nth-child("+i+")").fadeIn('slow').delay(2000); // Aumentei o tempo de espera para 2000ms
+				if(i==73){ // Alterei o valor para refletir o número total de parágrafos
+					$("p:nth-child(72)").fadeOut('slow').promise().done(function () {
+						$('.cake').fadeIn('fast');
+					});
+					
+				}
+				else{
+					msgLoop(i);
+				}            
+	
+			});
 		}
 		
 		msgLoop(0);
 		
 	});
-});
-
-// Adicione um evento de clique ao botão "Vamos a Decorar"
-$('#bannar_coming').click(function(){
-    $('.bannar').addClass('bannar-come');
-    $(this).fadeOut('slow').delay(6000).promise().done(function(){
-        $('#balloons_flying').fadeIn('slow');
-    });
-});
-
-$(document).ready(function(){
-    setTimeout(function() {
-        $('.bannar').addClass('banner-come'); // Alterado de '.banner' para '.bannar'
-    }, 3000);
+	
+	
 });
 
 
 
-$('.banner').hide();
+
 //alert('hello');
